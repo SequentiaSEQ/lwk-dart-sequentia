6819de586e9f0f462da196d7fdfc8819d210a203
Mihailo Milenkovic <mihailo.milenkovic84@gmail.com>
2024-09-09 12:12:58 +0200

    Remove TLS

--- rust/src/api/types.rs
+++ rust/src/api/types.rs
@@ -298,7 +298,7 @@ pub struct Blockchain {}
 
 impl Blockchain {
     pub fn test(&self, electrum_url: String) -> anyhow::Result<(), LwkError> {
-        ElectrumClient::new(&lwk_wollet::ElectrumUrl::Tls(electrum_url, false))?;
+        ElectrumClient::new(&lwk_wollet::ElectrumUrl::Plaintext(electrum_url))?;
         Ok(())
     }
 }
